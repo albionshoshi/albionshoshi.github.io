51016a57396a2dee5715f35fc2737dc14a6738a0
Albion Shoshi <albionshoshi@icloud.com>
2024-04-02 22:23:22 -0400

    again :(

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -6,7 +6,10 @@ function Home() {
             <div className="description">
                 <img className="pfp" src = "/zero.png"/>
                 <p className="home_desc">
-                    Hi my name is Albion Shoshi!
+                    Hi my name is Albion Shoshi, I'm currently a sophmore at Penn State University
+                    majoring in Data Sciences and a Minor in Statistics. I'm proficient in the
+                    languages C++ and Java but I like to explore as many branches as I can in the
+                    programming field.
                 </p>
             </div>
         </div>
