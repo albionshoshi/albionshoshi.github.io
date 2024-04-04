d61f58c17ebe16725459f06d65a70aa66325e5c6
Albion <albionshoshi@icloud.com>
2024-04-04 13:55:20 -0400

    yo

--- src/Components/Contract/Contact.js
+++ src/Components/Contract/Contact.js
@@ -5,6 +5,9 @@ function Contact() {
     return (
         <div>
             <h1>Contact Me</h1>
+            <a href="https://www.linkedin.com/in/albion-shoshi-334182252/">
+                <img className= "LinkedIn" src="/linkedInPFP.png"/>
+            </a>
         </div>
     )
 }
