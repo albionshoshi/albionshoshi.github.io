e300fd7a8eff61fcfbe83db538061810db5c2c6c
Albion <albionshoshi@icloud.com>
2024-04-06 19:39:21 -0400

    animation and trying to fix home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,20 +1,31 @@
-import "./Home.css"
+import React, { useState, useEffect } from 'react';
+import './Home.css';
+
 function Home() {
+    const fullText = "Hi my name is Albion Shoshi, I'm a rising junior attending Penn State University majoring in Data Sciences and have a passion for creating.";
+    const [text, setText] = useState('');
+
+    useEffect(() => {
+        let index = 0;
+        const intervalId = setInterval(() => {
+            setText((currentText) => currentText + fullText.charAt(index - 1));
+            index++;
+            if (index === fullText.length) clearInterval(intervalId);
+        }, 100);
+
+        return () => clearInterval(intervalId);
+    }, [fullText]);
+
     return (
-        <body className="homeBod">
-        <div>
-            <h1 className="tit">Home</h1>
-            <div className="description">
-                <img className="pfp" src = "/goat.jpg"/>
-                <p className="home_desc">
-                    Hi my name is Albion Shoshi, I'm currently a sophmore at Penn State University
-                    majoring in Data Sciences and a Minor in Statistics. I'm proficient in the
-                    languages C++ and Java but I like to explore as many branches as I can in the
-                    programming field.
-                </p>
+        <div className="homeBod">
+            <div>
+                <h1 className="tit">Home</h1>
+                <div className="description">
+                    <img className="pfp" src="/goat.jpg" alt="Profile"/>
+                    <p className="home_desc">{text}</p>
+                </div>
             </div>
         </div>
-        </body>
     );
 }
 
