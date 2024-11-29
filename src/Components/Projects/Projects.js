d629b75ab6bc4e255322cd98c7a9ac1adb2251e6
Albion <albionshoshi@icloud.com>
2024-11-28 20:58:35 -0500

    added more to projects

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -18,10 +18,23 @@ function Projects() {
         <div>
             <h1 className="projectHeader">Some of my notable projects</h1>
             <ProjectCard media={
-                <ReactPlayer url="ChessVID.mov" controls={false} playing={true}/>
+                <ReactPlayer url="ChessVID.mov" controls={false} playing={true} loop={true}/>
             } header4="Chess AI Agent" description="Created a chess AI with transitional
             functions able to play against 750 plus rated players"/>
+
+            <hr className="separator" />
+
             <ProjectCard header4= "Tic-Tac-Toe AI Agent" description={"Created a Tic-Tac-Toe AI with transitional functions. It plays as optimally as possible given its position on the board"}/>
+
+            <hr className="separator" />
+
+            <ProjectCard media={
+                <ReactPlayer url="Neurosis.webm" controls={false} playing={true} loop={true} muted={true} width="100%" height="100%"/>
+            } header4="Neurosis" description="Neurosis, a psychological horror game that takes place in our main characters brain. Our character goes into a journey of discovering who he truly is as he matches up with some of his greatest nightmares"/>
+
+            <hr className="separator" />
+
+            <ProjectCard header4= "Lebron Vs. Jordan Statistcal Summary" description={"A dive deep and settling the goat debate by comparing head to head stats between Michael Jordan and LeBron James"}/>
         </div>
     )
 }
